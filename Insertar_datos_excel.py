df7fd93f275023c40257d536562e92e7b4f5d2a1
Benjamin Polanco <115376642+Stellexd@users.noreply.github.com>
2025-07-04 02:14:11 -0400

    conexion a base de datos mala 04.07

--- Insertar_datos_excel.py
+++ Insertar_datos_excel.py
@@ -2,17 +2,17 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # 1. Cargar el Excel
-df = pd.read_excel(".xlsx")  # Aqui podemos cambiar el nombre de nuestro excel antes del punto
+df = pd.read_excel("ALEJANDRO CAMILO PALMA YAÑEZ Pruebas.xlsx")  # Aqui podemos cambiar el nombre de nuestro excel antes del punto
 
 # 2. Conexión a PostgreSQL (es obvio lo que hay que poner)
-usuario = "tu_usuario"
-contrasena = "tu_contraseña"
-host = "localhost"
+usuario = "benjamin"
+contrasena = "Ben1258"
+host = "192.168.100.149"
 puerto = "5432"
-base_datos = "nombre_basedatos"
+base_datos = "cubicanet"
 
 # 3. Crear la conexión usando SQLAlchemy (lo mismo de arriba)
 engine = create_engine(f'postgresql+psycopg2://{usuario}:{contrasena}@{host}:{puerto}/{base_datos}')
 
 # 4. Insertar los datos en la tabla (' ' aqui tenemos que poner el nombre de la tabla)
-df.to_sql('', engine, if_exists='append', index=False)
+df.to_sql('cubicanet', engine, if_exists='append', index=False)
